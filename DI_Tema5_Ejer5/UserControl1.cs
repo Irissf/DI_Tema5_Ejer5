6ed1ddcfb5895c3f9bce33c880674fd6eef2f136
Irissf <iris.seijo@gmail.com>
2021-02-23 16:05:15 +0100

    Agregar archivos de proyecto.

---
+++ DI_Tema5_Ejer5/UserControl1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DI_Tema5_Ejer5
+{ 
+    public enum eTipo
+    {
+        NUMERICO,TEXTUAL
+    }
+    public partial class UserControl1 : UserControl
+    {
+        Color color = Color.Red;
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        /*===============================================================================
+                    ___  ____ ____ ___  _ ____ ___  ____ ___  ____ ____ 
+                    |__] |__/ |  | |__] | |___ |  \ |__| |  \ |___ [__  
+                    |    |  \ |__| |    | |___ |__/ |  | |__/ |___ ___]
+
+        ===============================================================================*/
+        [Category("aa")]
+        [Description("Acceder a txt del textbox")]
+        public string Texto
+        {
+            set
+            {
+                textBox1.Text = value;
+                Refresh();
+            }
+            get
+            {
+                return textBox1.Text;
+            }
+        }
+
+        [Category("aa")]
+        [Description("Acceder al multiline del texbox")]
+        public bool Multilinea
+        {
+            set
+            {
+                textBox1.Multiline = value;
+                Refresh();
+            }
+            get
+            {
+                return textBox1.Multiline;
+            }
+        }
+
+        private eTipo tipoDeDato = eTipo.TEXTUAL;
+        [Category("aa")]
+        [Description("numérico o textual")]
+        public eTipo TipoDeDato
+        {
+            set
+            {
+                tipoDeDato = value;
+                Refresh();
+            }
+            get
+            {
+                return tipoDeDato;
+            }
+        }
+
+        /*===============================================================================
+                            ____ ____ ____ ____ _    ____ ____ ____ ____ 
+                            |__/ |___ |    |  | |    |  | |    |__| |__/ 
+                            |  \ |___ |___ |__| |___ |__| |___ |  | |  \
+         
+         ===============================================================================*/
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            textBox1.Location = new Point(10, 10);
+            this.Height = textBox1.Height + (20);
+            textBox1.Width = this.Width - (20);
+
+            string cadena = Texto.Trim();
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (tipoDeDato == eTipo.NUMERICO)
+                {
+                    if (cadena[i] >= 48 && cadena[i] <= 57)
+                    {
+                        color = Color.Green;
+                    }
+                    else
+                    {
+                        color = Color.Red;
+                    }
+                }
+                else
+                {
+                    if (cadena[i] >= 65 && cadena[i] <= 90 || cadena[i] >= 97 && cadena[i] <= 122)
+                    {
+                        color = Color.Green;
+                    }
+                    else
+                    {
+                        color = Color.Red;
+                    }
+                }
+            }
+
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(color);
+
+            g.DrawRectangle(pen, 5, 5, textBox1.Width + 10, textBox1.Height + 10);
+        }
+
+        private void TEXTcHANGE(object sender, EventArgs e)
+        {
+            this.OnTextChanged(e);
+        }
+    }
+
+}
---
+++ Prueba/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new DI_Tema5_Ejer5.UserControl1();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(358, 232);
+            this.userControl11.Multilinea = false;
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(247, 40);
+            this.userControl11.TabIndex = 0;
+            this.userControl11.Texto = "";
+            this.userControl11.TipoDeDato = DI_Tema5_Ejer5.eTipo.TEXTUAL;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DI_Tema5_Ejer5.UserControl1 userControl11;
+    }
+}
+
